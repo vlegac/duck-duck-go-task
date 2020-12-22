905ba8bf2f74f428ebb3ba43bd5a09420debd169
vlegac <vlegac@iolap.com>
2020-12-22 20:29:01 +0100

    Removed functions save/append and imported as helpers

--- api/routes/duck.js
+++ api/routes/duck.js
@@ -2,31 +2,7 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 const fs = require("fs");
-
-// Save into file if there is no history.json
-const saveQuery = (query) => {
-  let searchQuerys = {
-    searchedTerms: [],
-  };
-  searchQuerys.searchedTerms.push({ term: query });
-  let saveQueryData = JSON.stringify(searchQuerys, null, 2);
-  fs.writeFileSync("api/history/searchedHistory.json", saveQueryData);
-};
-
-// Read and append into history.json
-const appendQuery = (query) => {
-  let searchQuerys = {
-    searchedTerms: [],
-  };
-  console.log("here");
-
-  const historyData = fs.readFileSync("api/history/searchedHistory.json");
-
-  searchQuerys = JSON.parse(historyData);
-  searchQuerys.searchedTerms.push({ term: query });
-  let saveQueryData = JSON.stringify(searchQuerys, null, 2);
-  fs.writeFileSync("api/history/searchedHistory.json", saveQueryData);
-};
+const helpers = require("../../helpers/historyHelpers");
 
 router.get("/:term", async (req, res, next) => {
   let data;
@@ -45,9 +21,9 @@ router.get("/:term", async (req, res, next) => {
 
   const fileExists = fs.existsSync("api/history/searchedHistory.json");
   if (fileExists) {
-    appendQuery(query);
+    helpers.appendQuery(query);
   } else {
-    saveQuery(query);
+    helpers.saveQuery(query);
   }
   res.status(200).json(data);
 });
