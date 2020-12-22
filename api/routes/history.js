c5d13739afea2cec919125dac5553b3421fdf21c
vlegac <vlegac@iolap.com>
2020-12-22 20:29:12 +0100

    Removed clg

--- api/routes/history.js
+++ api/routes/history.js
@@ -6,7 +6,6 @@ const fs = require("fs");
 router.get("/", async (req, res, next) => {
   let historyRawData = fs.readFileSync("api/history/searchedHistory.json");
   let savedQuery = JSON.parse(historyRawData);
-  console.log(savedQuery);
   res.status(200).json(savedQuery);
 });
 
