7ace36450694bf8e62158702a057d425725052d1
vlegac <vlegac@iolap.com>
2020-12-22 21:54:41 +0100

    POST method

--- api/routes/duck.js
+++ api/routes/duck.js
@@ -4,17 +4,38 @@ const router = express.Router();
 const fs = require("fs");
 const helpers = require("../../helpers/historyHelpers");
 
+// GET
 router.get("/:term", async (req, res, next) => {
   let query = req.params.term;
   const [data, error] = await proxyHelper.proxy(query);
 
-  const fileExists = fs.existsSync("api/history/searchedHistory.json");
-  if (fileExists) {
-    helpers.appendQuery(query);
+  if (error) {
+    res.status(500).json({ error: error });
   } else {
-    helpers.saveQuery(query);
+    const fileExists = fs.existsSync("api/history/searchedHistory.json");
+    if (fileExists) {
+      helpers.appendQuery(query);
+    } else {
+      helpers.saveQuery(query);
+    }
+    res.status(200).json(data);
   }
-  res.status(200).json(data);
+});
+// POST
+router.post("/", async (req, res, next) => {
+  let query = req.body.term;
+  const [data, error] = await proxyHelper.proxy(query);
+  if (error) {
+    res.status(500).json({ error: error });
+  } else {
+    const fileExists = fs.existsSync("api/history/searchedHistory.json");
+    if (fileExists) {
+      helpers.appendQuery(query);
+    } else {
+      helpers.saveQuery(query);
+    }
+    res.status(200).json(data);
+  }
 });
 
 module.exports = router;
