8b96634a8cae1ffc5f7b697e6717137c201b16ea
vlegac <vlegac@iolap.com>
2020-12-23 18:59:07 +0100

    Fetch topics

--- client/src/redux/reducers/topicsReducer.js
+++ client/src/redux/reducers/topicsReducer.js
@@ -1,16 +1,14 @@
-const initialState = [
-  {
-    title: "Some title",
-    url: "url url",
-  },
-  {
-    title: "Some title2",
-    url: "url url",
-  },
-];
+const initialState = [];
 
 const topicsReducer = (state = initialState, action) => {
-  return state;
+  const { type, payload } = action;
+
+  switch (type) {
+    case "FETCH_TOPICS":
+      return payload;
+    default:
+      return state;
+  }
 };
 
 export default topicsReducer;
