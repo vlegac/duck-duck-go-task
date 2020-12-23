c56d8eac8107180fc750712db0fcecc4376acea5
vlegac <vlegac@iolap.com>
2020-12-23 17:45:18 +0100

    App init setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Topics } from "./components/Topics/Topics";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Search</h2>
+      <input type="text" id="search" name="search" />
+      <button>Search</button>
+      <Topics />
     </div>
   );
 }
