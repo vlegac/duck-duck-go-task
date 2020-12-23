bbab75a4e1b42d14c7cd0af6b7534269212d8b73
vlegac <vlegac@iolap.com>
2020-12-23 21:25:12 +0100

    Topic rework

--- client/src/components/Topics/Topic/Topic.jsx
+++ client/src/components/Topics/Topic/Topic.jsx
@@ -1,6 +1,31 @@
 import React from "react";
-import Link from "@material-ui/core/Link";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+
+import { topicStyles } from "./styles";
 
 export const Topic = ({ title, url }) => {
-  return <Link href={url}>{title}</Link>;
+  const classes = topicStyles();
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              {title.substring(0, 2)}
+            </Avatar>
+          }
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 };
