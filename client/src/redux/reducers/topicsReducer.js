1a1cdf9e7c0293a746c7225e910a30e22e520724
vlegac <vlegac@iolap.com>
2020-12-23 17:45:40 +0100

    RootReducer

---
+++ client/src/redux/reducers/topicsReducer.js
@@ -0,0 +1,16 @@
+const initialState = [
+  {
+    title: "Some title",
+    url: "url url",
+  },
+  {
+    title: "Some title2",
+    url: "url url",
+  },
+];
+
+const topicsReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default topicsReducer;
