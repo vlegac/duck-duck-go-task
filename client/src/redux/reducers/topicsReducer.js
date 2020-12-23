fefcd581f924963f1c4f0cb38264fea3d979ae54
vlegac <vlegac@iolap.com>
2020-12-23 21:23:06 +0100

    Reducers update

--- client/src/redux/reducers/topicsReducer.js
+++ client/src/redux/reducers/topicsReducer.js
@@ -1,11 +1,16 @@
-const initialState = [];
+const initialState = {
+  topics: [],
+  searchValue: "",
+};
 
 const topicsReducer = (state = initialState, action) => {
   const { type, payload } = action;
 
   switch (type) {
     case "FETCH_TOPICS":
-      return payload;
+      return { ...state, topics: payload };
+    case "HISTORY_FETCH_TOPICS":
+      return { ...state, searchValue: payload };
     default:
       return state;
   }
